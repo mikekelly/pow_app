6b7593993b6b9ca804219a17382bce20e097c049
Jamie Phelps <jamie@epiphanymedia.net>
2011-07-21 10:45:34 -0500

    Linking up default app if present

--- pow_app.rb
+++ pow_app.rb
@@ -4,6 +4,14 @@ require 'pathname'
 class PowApp < Sinatra::Base
   get('/') do
     @apps = Pathname.new(ENV['HOME'] + '/.pow').children.select { |c| c.directory? }
+    @default = nil
+    begin
+      default_path = Pathname.new(File.expand_path("~/.pow/default"))
+      if File.exist?(default_path) && default_path.ftype == "link"
+        @default = default_path.readlink.basename
+      end
+    rescue #Rescue because readlink and friends might not be available on the system.
+    end
     erb :index
   end
 end
